cb8b004e00d173fffeb3a888ebf86dfc9cb14be8
fsolans <francisco.solans@neodoo.es>
2015-01-26 21:49:36 +0100

    Incorporación de la creación de un proyecto básico con Maven.

---
+++ src/main/java/es/open4job/aytozgz/opendata/App.java
@@ -0,0 +1,13 @@
+package es.open4job.aytozgz.opendata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
